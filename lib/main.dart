ee619bf230bd96dce3508f48cc509032dfe41129
Александр Утбанов <utbanovag@it2g.ru>
2021-09-16 11:14:26 +0300

    add envs

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:it2g_calendar_mobile/navigation/navigation.dart';
 import 'package:it2g_calendar_mobile/navigation/navigation_container.dart';
 import 'package:it2g_calendar_mobile/store/storage.dart';
 import 'package:it2g_calendar_mobile/store/store.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
   runApp(App());
 }
 
--- lib/shared/constants/api_urls.dart
+++ lib/shared/constants/api_urls.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ApiUrls {
-  static final String baseUrl = '';
+  static final String baseUrl = dotenv.env['API_BASE_URL'] ?? '';
   static final String auth = '$baseUrl/auth';
   static final String logo = '$baseUrl/settings/logo/logo_it2g.png';
   static final String tasksEmployee = '$baseUrl/tasks/tasks-employee/';
