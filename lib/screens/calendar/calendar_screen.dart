5b2209f4659c7f16806f1942a87575323eb80382
UtbanovAG <utbanovag@it2g.ru>
2021-12-15 17:21:49 +0300

    return calendar navbar style

--- lib/screens/calendar/calendar_screen.dart
+++ lib/screens/calendar/calendar_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:work_calendar/screens/calendar/components/calendar.dart';
@@ -23,40 +24,13 @@ class CalendarScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Flex(
-        direction: Axis.vertical,
-        children: [
-          Expanded(
-            flex: 0,
-            child: Container(
-              color:  Colors.white,
-              padding: const EdgeInsets.only(top: 20, bottom: 10, left: 15, right: 15,),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  GestureDetector(
-                    onTap: () { _openCalendarForm(context); },
-                    child: const Text('Добавить', style: TextStyle(fontSize: 16),),
-                  ),
-                ]
-              ) ,
-            ), 
-            
-          ),
-          Expanded(
-            flex: 1,
-            child:  Calendar(profile: profile, tasks: tasks,)
-          )
-        ],
-      ))
+      appBar: CupertinoNavigationBar(
+        trailing: GestureDetector(
+          onTap: () { _openCalendarForm(context); },
+          child: const Text('Добавить'),
+        ),
+      ),
+      body: Calendar(profile: profile, tasks: tasks,)
     );
-    // return Scaffold(
-        // appBar: CupertinoNavigationBar(
-        //   trailing: GestureDetector(
-        //     onTap: () { _openCalendarForm(context); },
-        //     child: const Text('Добавить'),
-        //   ),
-        // ),
-    //     body: Calendar(profile: profile, tasks: tasks,));
   }
 }
